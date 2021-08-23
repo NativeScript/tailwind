2865e5175f87ea4e33c45dabf31385d8c1af4c7b
Igor Randjelovic <rigor789@gmail.com>
2021-08-23 16:55:07 +0200

    chore: add demo app

---
+++ demo/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './app/**/*.{css,xml,ts}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
