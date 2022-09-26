5250b71f8e000ebf35d2beb0aa5225b5bdbdbd2e
Igor Randjelovic <rigor789@gmail.com>
2022-09-26 14:29:46 +0200

    fix: ignore animation parsing errors
    closes #152, #153

--- src/expandAnimations.js
+++ src/expandAnimations.js
@@ -1,41 +1,45 @@
 const { parseSingle, serialize } = require("@hookun/parse-animation-shorthand");
 
 /**
- * @param {@} options 
+ * @param {@} options
  * @returns {import('postcss').Plugin}
  */
 module.exports = (options = { debug: false }) => {
   return {
     postcssPlugin: "postcss-nativescript-animations",
     Declaration(decl) {
-      // replace animation: *
-      // with    animation-*:
-      if (decl.prop === "animation") {
-        const styles = parseSingle(decl.value);
-        if (styles.duration && Number.isInteger(styles.duration)) {
-          styles.duration = `${styles.duration / 1000}s`;
+      try {
+        // replace animation: *
+        // with    animation-*:
+        if (decl.prop === "animation") {
+          const styles = parseSingle(decl.value);
+          if (styles.duration && Number.isInteger(styles.duration)) {
+            styles.duration = `${styles.duration / 1000}s`;
+          }
+          Object.entries(styles)
+            .filter(([, value]) => typeof value === "object")
+            .map(([key, value]) => [
+              key,
+              serialize({ [key]: value }).split(" ")[0],
+            ])
+            .map(([key, value]) => (styles[key] = value));
+
+          Object.entries(styles)
+            .filter(([, value]) => value !== "unset")
+            .map(([key, value]) =>
+              decl.parent.insertAfter(
+                decl,
+                decl.clone({
+                  prop: `animation-${camelToKebab(key)}`,
+                  value: `${value}`,
+                })
+              )
+            );
+
+          decl.remove();
         }
-        Object.entries(styles)
-          .filter(([, value]) => typeof value === "object")
-          .map(([key, value]) => [
-            key,
-            serialize({ [key]: value }).split(" ")[0],
-          ])
-          .map(([key, value]) => (styles[key] = value));
-
-        Object.entries(styles)
-          .filter(([, value]) => value !== "unset")
-          .map(([key, value]) =>
-            decl.parent.insertAfter(
-              decl,
-              decl.clone({
-                prop: `animation-${camelToKebab(key)}`,
-                value: `${value}`,
-              })
-            )
-          );
-
-        decl.remove();
+      } catch (err) {
+        // ignore errors, and just keep the animation as-is
       }
     },
   };
